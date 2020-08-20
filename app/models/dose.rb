883d6ccea99e84e26d3eb13c544f034e0bcaaeac
leandro barroso <lrbarroso1984@gmail.com>
2020-08-20 12:06:21 -0300

    Models relations and validtions ok

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient, message: 'should has one per ingredient' }
 end
