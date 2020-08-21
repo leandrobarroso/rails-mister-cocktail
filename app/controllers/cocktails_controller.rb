f2a51d31325ca08ae755b8e6b1f250f50b03c8bd
leandro barroso <lrbarroso1984@gmail.com>
2020-08-21 18:58:16 -0300

    Upload image feature added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -35,6 +35,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
