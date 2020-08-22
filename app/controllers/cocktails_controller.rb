0c7fe6228d326ca6b0d535cab445bec5a98cd386
leandro barroso <lrbarroso1984@gmail.com>
2020-08-22 00:28:40 -0300

    search cocktail by name feature added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,7 +2,11 @@ class CocktailsController < ApplicationController
   before_action :set_cocktail, only: %i[show destroy]
 
   def index
-    @cocktails = Cocktail.all
+    if params[:query].present?
+      @cocktails = Cocktail.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @cocktails = Cocktail.all
+    end
   end
 
   def show
